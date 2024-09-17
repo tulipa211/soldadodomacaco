573479911d72117d563e97483b2f9ef1f30e2012
tulipa211 <159141887+tulipa211@users.noreply.github.com>
2024-09-17 08:26:11 -0300

    k

---
+++ flashcards/pergunta.js
@@ -0,0 +1,90 @@
+criaCartao(
+    'Física',
+    'Qual a fórmula do calor específico?',
+    'ΔQ = m·c·ΔT'
+)
+criaCartao(
+    'Biologia',
+    'Qual o processo em que ocorre o acúmulo de uma substância nos níveis tróficos da cadeia alimentar?',
+    'Magnificação trófica'
+)
+criaCartao(
+    'Matemática',
+    'Qual o papel do coeficiente linear na função do 1º Grau?',
+    'Ponto em que a reta corta o eixo Y'
+)
+criaCartao(
+    'Química',
+    'O que significa a tetravalência do Carbono?',
+    'Que o carbono é capaz de fazer quatro ligações'
+)
+criaCartao(
+    'Biologia',
+    'Qual são as principais etapas da respiração celular',
+    'Glicólise, Ciclo de Krebs e Cadeia Respiratória'
+)
+criaCartao(
+    'Biologia',
+    'Qual o ácido sintetizado no processo da glicólise?',
+    'Ácido pirúvico'
+)
+criaCartao(
+    'Matemática',
+    'Qual a fórmula do volume de um cone?',
+    'V=Ab·h·1/3'
+)
+criaCartao(
+    'Físico-química',
+    'Qual o nome do processo de passagem de um sólido para o líquido?',
+    'Fusão'
+)
+criaCartao(
+    'Biologia',
+    'Qual a função das enzimas no corpo-humano?',
+    'Catalisadora de reações bio-químicas'
+)
+criaCartao(
+    'Biologia',
+    'Estrutura da planta responsável do auxílio da penetração do solo e proteção da região meristemática?',
+    'Coifa'
+)
+criaCartao(
+    'Física',
+    'Qual a principal propriedade de uma onda mecânica?',
+    'Necessidade de matéria para sua propagação'
+)
+criaCartao(
+    'Física',
+    'Um espelho convexo forma qual tipo de imagem?',
+    'Virtual'
+)
+criaCartao(
+    'Química',
+    'Qual a lei da conservação da matéria proposta por Lavousier?',
+    'Nada se cria nada se perde, tudo se transforma'
+)
+criaCartao(
+    'Física',
+    'Qual a fórmula da resistividade de um material?',
+    'R=ρ⋅L/A'
+)
+criaCartao(
+    'Matemática',
+    'Quanto vale 1m³ em litros?',
+    '1000 Litros'
+)
+criaCartao(
+    'Física',
+    'Qual a fórmula da Força Elétrica?',
+    'F=k.|Q1|·|Q2|/d²'
+)
+criaCartao(
+    'Química',
+    'Qual a partícula radioativa que não possui massa?',
+    'Partículas Gama (γ) '
+)
+criaCartao(
+    'Matemática',
+    'Qual a fórmula do Xv?',
+    'Xv=-b/2a'
+)
