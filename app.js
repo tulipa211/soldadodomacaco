97a0857750aa704b45bdea9e5bfe58b688c9d6d9
tulipa211 <159141887+tulipa211@users.noreply.github.com>
2024-10-03 08:18:17 -0300

    .

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ function criaCartao(categoria, pergunta, resposta){
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-    cartao.innerHTML=`
-    <div class="cartao-conteudo">
-    <h3> ${categoria} </h3>
-    <div class="cartao-pergunta">
-        <p> ${pergunta} </p>
+    cartao.innerHTML = `
+    <div class = "cartao-conteudo">
+    <h3>${categoria}</h3>
+    <div class = "cartao-pergunta">
+        <p>${pergunta}</p>
     </div>
-    <div class="cartao-reposta">
-        <p> ${resposta} </p>
+    <div class="cartao-resposta">
+        <p>${resposta}</p>
     </div>
-     </div>
+    </div>
     `
     container.appendChild(cartao);
+
 }
