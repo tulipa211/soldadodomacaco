4d0706450b36be68a2323a9acb8fd1d57509bfa2
tulipa211 <159141887+tulipa211@users.noreply.github.com>
2024-10-03 08:24:38 -0300

    .

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ function criaCartao(categoria, pergunta, resposta){
     </div>
     </div>
     `
-    container.appendChild(cartao);
+
+
+let respostaVisivel = false;
+
+function viraCartao(){
+    respostaVisivel = !respostaVisivel;
+    cartao.classList.toggle('active', respostaVisivel);
+}
+
+cartao.addEventListener('click', viraCartao);
+
+container.appendChild(cartao);
 
 }
